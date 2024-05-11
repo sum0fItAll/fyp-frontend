6a5d62fa4e74aa60314d1e92b8fbf61bd33861a8
Cloud-Bomb <ahsan_faizan@outlook.com>
2024-05-11 20:48:03 +0500

    added index.ts to export all screens

---
+++ app/screens/index.ts
@@ -0,0 +1,12 @@
+// screens/index.ts
+export { default as LandingPage } from './LandingPage';
+export { default as LoginPage } from './LoginPage';
+export { default as SignUpPage } from './SignUpPage';
+export { default as HomePage } from './HomePage';
+export { default as SearchPage } from './SearchPage';
+export { default as ForumPage } from './ForumPage';
+export { default as ProfilePage } from './ProfilePage';
+export { default as SettingsPage } from './SettingsPage';
+export { default as ProfilePageSecond } from './ProfilePageSecond';
+export { default as LoginPageSecond } from './LoginPageSecond';
+export { default as BookDetailsPage } from './BookDetailsPage';
